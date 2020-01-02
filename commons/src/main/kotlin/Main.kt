f517d18e8bfd1ea2810acc4cdd264b5c01d4dc5f
Yaison <yaison1708@gmail.com>
2020-01-01 23:18:07 -0400

    Testing Grid.

--- commons/src/main/kotlin/Main.kt
+++ commons/src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
-
+import com.gbc.commons.structs.Grid
 
 fun main(args: Array<String>){
-    println("Hello world!!")
+    val g = Grid(3, 3)
+
+    g.header(0, "Pop")
+    g[0, 0] = 7
+    g[0, 1] = 7.1
+    g[0, 1] = 72325656763.232343
+    g[1,1] = Double.NaN
+    g[2, 2] = 1/2.0
+    g[1, 2] = "sdjaksdsads sdsa    "
+    g.print()
 }
